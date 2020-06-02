af97a295ccd9b1ebc16ba07faaa53a7304b77659
luizlaljr <luizlaljr@gmail.com>
2020-06-02 10:40:23 -0300

    Refactored Login Page

--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -17,133 +17,148 @@ class _LoginPageState extends ModularState<LoginPage, LoginController> {
       body: Container(
         color: Color(0xFF12365A),
         child: Padding(
-          padding: const EdgeInsets.all(35.0),
+          padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 10),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
+              Container(),
               Container(
+                width: 200,
+                height: 160,
                 decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    stops: [0.0, 0.6],
-                    colors: [
-                      Color(0xFF2E4C6E),
-                      Color(0xFF1C2C43),
-                    ],
-                  ),
-                  borderRadius: BorderRadius.circular(30),
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/gladio.png'),
+                      fit: BoxFit.fill),
                 ),
-                child: TextField(
-                  onChanged: (value) {
-                    controller.setEmail(value);
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1.3),
-                  textAlign: TextAlign.center,
-                  cursorColor: Colors.white,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(
-                      Icons.person_outline,
-                      color: Colors.white38,
-                    ),
-                    suffixText: "@fab.mil.br",
-                    suffixStyle: TextStyle(
-                      color: Colors.white54,
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 15,
               ),
               Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    stops: [0.0, 0.6],
-                    colors: [
-                      Color(0xFF2E4C6E),
-                      Color(0xFF1C2C43),
-                    ],
-                  ),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: TextField(
-                  onChanged: controller.setPassword,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1.1),
-                  obscureText: true,
-                  cursorColor: Colors.white,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(
-                      Icons.lock_outline,
-                      color: Colors.white38,
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
+                  child: Column(
                 children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      height: 40,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.centerLeft,
-                          end: Alignment.centerRight,
-                          stops: [0.1, 1],
-                          colors: [
-                            Color(0xFF4782FB),
-                            Color(0xFF00E6FD),
-                          ],
+                  Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        stops: [0.0, 0.6],
+                        colors: [
+                          Color(0xFF2E4C6E),
+                          Color(0xFF1C2C43),
+                        ],
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: TextField(
+                      onChanged: (value) {
+                        controller.setEmail(value);
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.3),
+                      textAlign: TextAlign.center,
+                      cursorColor: Colors.white,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.person_outline,
+                          color: Colors.white38,
                         ),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(30),
+                        suffixText: "@fab.mil.br",
+                        suffixStyle: TextStyle(
+                          color: Colors.white54,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(30),
                         ),
                       ),
-                      child: FlatButton(
-                        child: Text(
-                          'Login',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        stops: [0.0, 0.6],
+                        colors: [
+                          Color(0xFF2E4C6E),
+                          Color(0xFF1C2C43),
+                        ],
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: TextField(
+                      onChanged: controller.setPassword,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.1),
+                      obscureText: true,
+                      cursorColor: Colors.white,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.lock_outline,
+                          color: Colors.white38,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          height: 40,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                              stops: [0.1, 1],
+                              colors: [
+                                Color(0xFF4782FB),
+                                Color(0xFF00E6FD),
+                              ],
+                            ),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(30),
+                            ),
+                          ),
+                          child: FlatButton(
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            textColor: Colors.white,
+                            onPressed: controller.login,
                           ),
                         ),
-                        textColor: Colors.white,
-                        onPressed: controller.login,
-                      ),
-                    ),
-                  )
+                      )
+                    ],
+                  ),
                 ],
-              )
+              )),
             ],
           ),
         ),
